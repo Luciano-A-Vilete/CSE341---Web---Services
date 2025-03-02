c338c8c7e3deea90973d1c3aee5bd3bb49d54b88
Luciano-A-Vilete <luciano.a.vilete@hotmail.com>
2025-03-02 00:09:08 -0300

    adjusting the gitignore file

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
-
 const express = require('express');
 const app = express();
-const lesson1Controller = require('./controllers/lesson1');
 
-app.get('/', (req, res) => {
-    res.send('Hello, Express.js server!');
-});
+app.use('/', require('./routes'));
 
 app.get('/lais', lesson1Controller.laisRoute);
 
 app.get('/vilete', lesson1Controller.vileteRoute);
 
-const port1 = 3000;
-app.listen(port1, () => {
-    console.log(`Server running at http://localhost:${port1}/`);
-});
+const port = 3000;
+app.listen(process.env.port || port);
