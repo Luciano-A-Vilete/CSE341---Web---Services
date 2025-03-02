7ac03173cba663c305691f1a7fb8c8de38a7ece3
Luciano-A-Vilete <luciano.a.vilete@hotmail.com>
2025-03-01 23:25:32 -0300

    new structure archives

---
+++ controllers/lesson1.js
@@ -0,0 +1,12 @@
+const laisRoute = (req, res) => {
+    res.send('Laís Vilete');
+}
+
+const vileteRoute = (req, res) => {
+    res.send('Luciano Vilete');
+}
+
+module.exports = {
+    laisRoute,
+    vileteRoute
+};
--- server.js
+++ server.js
@@ -1,25 +1,13 @@
-const http = require('http');
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, Node.js server!');
+const express = require('express');
+const app = express();
+const lesson1Controller = require('./controllers/lesson1');
+
+app.get('/', (req, res) => {
+    res.send('Hello, Express.js server!');
 });
 
-const port = 3000;
-server.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+const port1 = 3000;
+app.listen(port1, () => {
+    console.log(`Server running at http://localhost:${port1}/`);
 });
-
-
-
-//const express = require('express');
-//const app = express();
-
-//app.get('/', (req, res) => {
-//    res.send('Hello, Express.js server!');
-//});
-
-//const port1 = 3000;
-//app.listen(port1, () => {
-//    console.log(`Server running at http://localhost:${port1}/`);
-//});
