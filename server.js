8776e7370289c9364c64bfc2711ea67a4050a196
Luciano-A-Vilete <luciano.a.vilete@hotmail.com>
2025-03-01 23:30:31 -0300

    adjustments on routing

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
     res.send('Hello, Express.js server!');
 });
 
+app.get('/lais', lesson1Controller.laisRoute);
+
+app.get('./vilete', lesson1Controller.vileteRoute);
+
 const port1 = 3000;
 app.listen(port1, () => {
     console.log(`Server running at http://localhost:${port1}/`);
